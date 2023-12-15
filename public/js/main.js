0fd846d2ea42589723e1ca76630e541cacf841da
ballack <rodrigue-dev>
2023-12-15 08:40:44 +0100

    ert

--- public/js/main.js
+++ public/js/main.js
@@ -528,7 +528,7 @@ $(document).ready(function(){
 	};
 	setInterval(timer, 1000);
 		// Set the date we're counting down to
-		var countDownDate = new Date("dec 12, 2023 12:00:00").getTime();
+		var countDownDate = new Date("dec 15, 2023 10:00:00").getTime();
 		// Update the count down every 1 second
 		var x = setInterval(function() {
 		// Get todays date and time
