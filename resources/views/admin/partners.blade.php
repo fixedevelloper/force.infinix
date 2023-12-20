8f8c41ad703b60fa1b4cf441fb007992922285f4
ballack <rodrigue-dev>
2023-12-20 07:18:36 +0100

    ert

--- resources/views/admin/partners.blade.php
+++ resources/views/admin/partners.blade.php
@@ -5,20 +5,23 @@
     <div class="card">
         <span hidden id="id_user_smart">{{$id}}</span>
         <div class="card-body">
-            <table class="table table-striped table-bordered" id="partner_table">
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>ID</th>
-                    <th>Address</th>
-                    <th>Current Level</th>
-                </tr>
-                </thead>
-                <tbody>
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered" id="partner_table">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>ID</th>
+                        <th>Address</th>
+                        <th>Current Level</th>
+                    </tr>
+                    </thead>
+                    <tbody>
 
-                </tbody>
+                    </tbody>
 
-            </table>
+                </table>
+            </div>
+
         </div>
     </div>
 
